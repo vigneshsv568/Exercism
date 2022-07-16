dcb5d885b403c9b72f9ead0e1a9afb81a7d7ab68
vigneshsv568 <vigneshsv568@gmail.com>
2022-07-16 12:11:58 +0530

    RNA Solution Added

--- rna-transcription/src/main/java/RnaTranscription.java
+++ rna-transcription/src/main/java/RnaTranscription.java
@@ -4,7 +4,21 @@ class RnaTranscription {
 
     String transcribe(String dnaString) {
 
-        
+        Hashtable<Character, Character> check = new Hashtable<Character, Character>();
+        String out = "";
+
+        check.put('G','C');
+        check.put('C', 'G');
+        check.put('A','U');
+        check.put('T','A');
+
+        for(int i=0;i<dnaString.length();i++){
+            char ch = check.containsKey(dnaString.charAt(i)) ? check.get(dnaString.charAt(i) ) : dnaString.charAt(i);
+            out += ch;
+        }
+        return out;
+
+
 
 
 
